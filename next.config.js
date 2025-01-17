fcc676f6f93d275cf5eea8a97c19e02451867755
huangjiajian <huangjiajian@csenergytech.com>
2025-01-18 04:42:08 +0800

    简化 Next.js 配置

--- next.config.js
+++ next.config.js
@@ -1,21 +1,12 @@
-const withMDX = require('@next/mdx')()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 启用 MDX 支持
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  
-  // 配置国际化
-  i18n: {
-    locales: ['default', 'es', 'ja', 'ko', 'pt-br', 'vi'],
-    defaultLocale: 'default'
-  },
 
   // 配置图片域名
   images: {
-    domains: ['localhost'],
     unoptimized: true
   }
 }
 
-module.exports = withMDX(nextConfig) 
+module.exports = nextConfig 
